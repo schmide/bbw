336cb53864f9db42ba1c3ef242a556842e8368ac
schmide <andrew@schmide.com>
2020-01-07 03:26:05 -0500

    Centered the signal
    1 1 2 -> 1.1 1.1 1.1

--- bbw.ino
+++ bbw.ino
@@ -17,6 +17,7 @@
 #define TIME() micros()
 #define INPUT_PIN 13
 #define OUTPUT_PIN 7
+#define DEFAULT_LOGIC true
 #define MSB_FIRST false
 // below derived on 16mhz mega
 #define PERIOD_BAUD 115200.0
@@ -26,8 +27,8 @@
 #define PERIOD_HIGH 115200.0
 #define PERIOD_CLOCK_SCALE_RATIO (100.0 * PERIOD_HIGH / PERIOD_MHZ)
 #define PERIOD_RATIO (1.0 + (PERIOD_CLOCK_SCALE_RATIO * (1.0 - (PERIOD_BAUD - PERIOD_LOW) / (PERIOD_HIGH - PERIOD_LOW))))
-#define PERIOD_SHIFT 0.0
 #define PERIOD_DELAY (PERIOD_RATIO * PERIOD / PERIOD_BAUD)
+#define PERIOD_SHIFT (0.0 * PERIOD_DELAY)
 #define PERIOD_SLEW (0.5 * PERIOD_DELAY)
 #define STARTDELAY (13 * PERIOD_DELAY)
 #define READTIMEOUT (32 * PERIOD_DELAY)
@@ -35,30 +36,30 @@
 #define SET_BIT(a) digitalWrite(OUTPUT_PIN, a)
 #define GET_BIT() digitalRead(INPUT_PIN)
 
-unsigned char sendBuffer[] = { 0xaa , 0x9c}; //, 0x55, 0xaa, 0x9c, 0x55}; //, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55 };
+unsigned char sendBuffer[] = { 0xaa , 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55 };
 unsigned char receiveBuffer[9];
 unsigned long defaultSendDelays[] = { \
-                                      (unsigned long)(1.0 * PERIOD_DELAY - (PERIOD_SHIFT / PERIOD_BAUD) ), \
-                                      (unsigned long)(1.0 * PERIOD_DELAY), \
-                                      (unsigned long)(2.0 * PERIOD_DELAY + (PERIOD_SHIFT / PERIOD_BAUD) ), \
+                                      (unsigned long)(1.1 * PERIOD_DELAY + PERIOD_SHIFT), \
+                                      (unsigned long)(1.1 * PERIOD_DELAY), \
+                                      (unsigned long)(1.1 * PERIOD_DELAY - PERIOD_SHIFT), \
                                     };
 
 void SendBytes(unsigned char *send, int sizeSend, unsigned long *delays = defaultSendDelays)
 {
   noInterrupts();
   unsigned char *end = &send[sizeSend];
-  SET_BIT(true); // sync to port
+  SET_BIT(DEFAULT_LOGIC); // sync to port
   do {
-    SET_BIT(false);
+    SET_BIT(!DEFAULT_LOGIC);
     DELAY(delays[0]);
     unsigned char bit = MSB_FIRST ? 1 << 7 : 1;
     do {
-      bool bitBool = *send & bit ? true : false;
+      bool bitBool = *send & bit ? DEFAULT_LOGIC : !DEFAULT_LOGIC;
       SET_BIT(bitBool);
-      MSB_FIRST ? bit >>= 1 : bit <<= 1;
       DELAY(delays[1]);
+      MSB_FIRST ? bit >>= 1 : bit <<= 1;
     } while (bit);
-    SET_BIT(true);
+    SET_BIT(DEFAULT_LOGIC);
     DELAY(delays[2]);
   } while (++send < end);
   interrupts();
@@ -68,7 +69,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(INPUT_PIN, INPUT);
   pinMode(OUTPUT_PIN, OUTPUT);
-  SET_BIT(true);
+  SET_BIT(DEFAULT_LOGIC);
   //  Serial1.begin(38400);
 }
 
@@ -88,5 +89,5 @@ void loop() {
     //    delay(10);
   }
   //  DELAY_UNTIL(startTime+1000);
-  delay(50);
+  delay(100);
 }
