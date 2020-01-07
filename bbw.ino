cc00ec001fd98760af13f96843d7cd4a57676593
schmide <andrew@schmide.com>
2020-01-06 20:42:24 -0500

    Delay and Linear Eq solve the problem
    Now runs at 16 * 9600 = 153600 without problem

--- bbw.ino
+++ bbw.ino
@@ -1,61 +1,68 @@
 // bbw.cpp : bit bang wheele
 //
 /*
-  Went back to using the clock. Works up to 57600 on a mega. Though 38400 is more reliable.
+  Delays are back in fashion.
 
-  The algorithm is basically a series of delays polling the clock while shifting out bits.
-  Using the clock keeps the frame in sync. However this is an integer operation and thus 
-  ratios are never exact. Long messages may need breaks.
+  While determining the delays between 9600 and 115200, a couple things happened.
+  First the ratio for 115200 was exactly 1.0. Second the ratio for 9600 was 0.72
+  So I started building a linear equation to translate mhz baud and period such 
+  that it would map from 9600-115200. As I was doing it the mhz and baud would
+  each become unnecessary. Well it turns out 115200 * 100 / 16mhz = 0.72 and
+  the max it will run at it 16 * 9600 = 153600. I can only assume the writing to 
+  the ports is keeping things synchronized. At 153600 the period delay it 4
+  communication have a base ratio equal to 1mhz. 
 
-  At 57600 you have 17.36 ticks per bit which causes a bit of jitter. Lowering the 
-  PERIOD_ADJUST may help a bit but errors were there. 38400 with 26 ticks a bit was
-  very consistant.
+  Actual PERIOD_DELAY is 2 nanoseconds or 1:500000
 */
 
 #define TIME() micros()
 #define INPUT_PIN 13
 #define OUTPUT_PIN 7
 #define MSB_FIRST false
-// below tested on 16mhz mega
-#define PERIOD_BAUD 38400.0
-#define PERIOD_ADJUST 1.0
-#define PERIOD_SHIFT 2.0 // shifts the data to the left 38400 likes 2-4 57600 5+
-#define PERIOD_DELAY_MICRO (1000000.0 * PERIOD_ADJUST / PERIOD_BAUD)
-#define PERIOD_SLEW (0.5 * PERIOD_DELAY_MICRO)
-#define STARTDELAY (13 * PERIOD_DELAY_MICRO)
-#define READTIMEOUT (32 * PERIOD_DELAY_MICRO)
-//#define DELAY(a) { unsigned long egg = TIME() + ((unsigned long)(a)); while (TIME() <= egg); }
-//#define DELAY(a) { unsigned long egg = ((unsigned long)(a); while (--egg); }
-#define DELAY_UNTIL(a) { unsigned long egg = (unsigned long)(a); while (TIME() <= egg); }
-#define DELAY_HAMMER(a, b) { unsigned char h=a; do { (b); } while(--h); } 
+// below derived on 16mhz mega
+#define PERIOD_BAUD 9600.0
+#define PERIOD_MHZ 16000000.0
+#define PERIOD 1000000.0
+#define PERIOD_LOW 9600.0
+#define PERIOD_HIGH 115200.0
+#define PERIOD_CLOCK_SCALE_RATIO (100.0 * PERIOD_HIGH / PERIOD_MHZ)
+#define PERIOD_RATIO (1.0 + (PERIOD_CLOCK_SCALE_RATIO * (1.0 - (PERIOD_BAUD - PERIOD_LOW) / (PERIOD_HIGH - PERIOD_LOW))))
+#define PERIOD_SHIFT 0.0
+#define PERIOD_DELAY (PERIOD_RATIO * PERIOD / PERIOD_BAUD)
+#define PERIOD_SLEW (0.5 * PERIOD_DELAY)
+#define STARTDELAY (13 * PERIOD_DELAY)
+#define READTIMEOUT (32 * PERIOD_DELAY)
+#define DELAY(a) { unsigned long egg = ((unsigned long)(a)); while (--egg) { __asm__("nop\n\t"); } }
 #define SET_BIT(a) digitalWrite(OUTPUT_PIN, a)
 #define GET_BIT() digitalRead(INPUT_PIN)
 
-unsigned char sendBuffer[] = { 0xaa }; //, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55 };
+unsigned char sendBuffer[] = { 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55, 0xaa, 0x9c, 0x55 };
 unsigned char receiveBuffer[9];
-unsigned int defaultSendDelays[] = { (unsigned int)(1.0 * PERIOD_DELAY_MICRO - PERIOD_SHIFT), (unsigned int)(1.0 * PERIOD_DELAY_MICRO), (unsigned int)(10.0 * PERIOD_DELAY_MICRO) };
+unsigned long defaultSendDelays[] = { \
+                                      (unsigned long)(1.0 * PERIOD_DELAY - (PERIOD_SHIFT / PERIOD_BAUD) ), \
+                                      (unsigned long)(1.0 * PERIOD_DELAY), \
+                                      (unsigned long)(2.0 * PERIOD_DELAY + (PERIOD_SHIFT / PERIOD_BAUD) ), \
+                                    };
 
-void SendBytes(unsigned char *send, int sizeSend, unsigned int *delays = defaultSendDelays)
+void SendBytes(unsigned char *send, int sizeSend, unsigned long *delays = defaultSendDelays)
 {
+  noInterrupts();
   unsigned char *end = &send[sizeSend];
   SET_BIT(true); // sync to port
-  unsigned long startTime = TIME();
   do {
     SET_BIT(false);
-    unsigned long nextTime = startTime+delays[0];
-    DELAY_UNTIL(nextTime);    
+    DELAY(delays[0]);
     unsigned char bit = MSB_FIRST ? 1 << 7 : 1;
     do {
       bool bitBool = *send & bit ? true : false;
       SET_BIT(bitBool);
-      nextTime += delays[1];
       MSB_FIRST ? bit >>= 1 : bit <<= 1;
-      DELAY_UNTIL(nextTime);    
+      DELAY(delays[1]);
     } while (bit);
     SET_BIT(true);
-    startTime+=delays[2];
-    DELAY_UNTIL(startTime);    
+    DELAY(delays[2]);
   } while (++send < end);
+  interrupts();
 }
 
 void setup() {
@@ -63,7 +70,7 @@ void setup() {
   pinMode(INPUT_PIN, INPUT);
   pinMode(OUTPUT_PIN, OUTPUT);
   SET_BIT(true);
-//  Serial1.begin(38400);
+  //  Serial1.begin(38400);
 }
 
 bool state = true;
@@ -72,15 +79,15 @@ void loop() {
   unsigned long startTime = TIME();
   if (true) {
     SendBytes(sendBuffer, sizeof(sendBuffer) / sizeof(sendBuffer[0]));
-//    DELAY(16);
-//    delay(10);
-//    Serial.print("Delay = ");
-//    Serial.println(((unsigned int)defaultSendDelays[3]));
+    //    DELAY(16);
+    //    delay(10);
+//        Serial.print("Delay = ");
+//        Serial.println(((unsigned int)defaultSendDelays[0]));
     //  ReceiveBytes(receiveBuffer, sizeof(receiveBuffer) / sizeof(receiveBuffer[0]));
   } else {
-//    Serial1.write(sendBuffer, sizeof(sendBuffer) / sizeof(sendBuffer[0]));
-//    delay(10);
+    //    Serial1.write(sendBuffer, sizeof(sendBuffer) / sizeof(sendBuffer[0]));
+    //    delay(10);
   }
-  DELAY_UNTIL(startTime+1000);    
-//    delay(10);
+  //  DELAY_UNTIL(startTime+1000);
+  delay(50);
 }
